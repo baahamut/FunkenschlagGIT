d4ad8716315c529302961f6e847dda730c06c88e
victor <victorwa@hotmail.fr>
2016-11-29 08:34:26 +0100

    Centrale.java
    La classe centrale

---
+++ Centrale.java
@@ -0,0 +1,12 @@
+
+public class Centrale {
+	private int valeur;
+	private String type;
+	private int alimente;
+	private boolean chargee;
+	private int capacite;
+	
+	public Marqueur ajouterRessource(int m){
+		
+	}
+}
