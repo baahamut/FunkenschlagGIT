29dfc3afa8c23b1b5b3fd2cb0466a4b6013ea2e7
victor <victorwa@hotmail.fr>
2016-11-29 09:30:04 +0100

    CentraleV1.1.java
    version modelio 1

--- Centrale.java
+++ Centrale.java
@@ -1,12 +1,36 @@
+package Model.MarcheCentrales;
 
-public class Centrale {
-	private int valeur;
-	private String type;
-	private int alimente;
-	private boolean chargee;
-	private int capacite;
-	
-	public Marqueur ajouterRessource(int m){
-		
-	}
+import java.util.ArrayList;
+import java.util.List;
+import Model.MarcheRessources.Marqueur;
+import com.modeliosoft.modelio.javadesigner.annotations.objid;
+
+@objid ("247a81dc-aeb6-4dbe-a515-7ee56d150730")
+public class Centrale extends Carte {
+    @objid ("f03f4719-38e3-40e8-988e-b3fae92e6afc")
+    private int valeur;
+
+    @objid ("c18ffb3e-4412-431c-bd45-f134e62bc4d4")
+    private String type;
+
+    @objid ("a595894a-8706-44eb-a538-fddd909952f4")
+    private int alimente;
+
+    @objid ("28184114-8fcc-4038-b0fb-49637043e803")
+    private boolean chargee;
+
+    @objid ("081ce1ae-4cc4-49c8-889e-afb6c4930289")
+    private int capacite;
+
+    @objid ("3caa0128-bea9-4828-9a91-5838b018f263")
+    public List<Marqueur> Marqueurs = new ArrayList<Marqueur> ();
+
+    @objid ("860a7f83-85b1-4cd8-8ee7-4971d7e05133")
+    public void ajouterRessources(Marqueur m) {
+    }
+
+    @objid ("68f49f02-383c-42ed-a70b-cb267e946703")
+    public void retirerRessources(Marqueur m) {
+    }
+
 }
