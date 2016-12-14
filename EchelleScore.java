bd479669f10f25e9139f47a93b3f54c3d87fa95f
victor <victorwa@hotmail.fr>
2016-12-14 10:09:07 +0100

    EchelleScoreV1
    document non modifier venant de modelio

---
+++ EchelleScore.java
@@ -0,0 +1,15 @@
+package Model;
+
+import com.modeliosoft.modelio.javadesigner.annotations.objid;
+
+@objid ("f832cc3e-f3a8-4a01-86de-d0b55a02a920")
+public class EchelleScore {
+    @objid ("ba2735c2-5027-493c-9ef2-3700a158e160")
+    public void mettreaJour(Joueur p[]) {
+    }
+
+    @objid ("43bab923-4f17-4726-ba23-df7e7d6d117e")
+    public EchelleScore() {
+    }
+
+}
